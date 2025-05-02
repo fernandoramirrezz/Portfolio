ea3ae187e29e267fa1b3df99164b8f0973c2da49
Fernando <fernandoramirez309210@gmail.com>
2025-05-02 15:20:47 -0500

    LIGHTSABER

--- src/app/site_components/LightSaber.jsx
+++ src/app/site_components/LightSaber.jsx
@@ -5,8 +5,8 @@ import { Lightsaber } from "@/components/ui/lightsaber";
 export function LightSaber () {
     
   return (
-    <div className="container">
-      <Lightsaber />
+    <div className="w-full flex justify-center">
+      <Lightsaber className="w-[80vw] max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl" />
     </div>
   );
 }
