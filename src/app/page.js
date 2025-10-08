7e10141b7b4dd592d9faad8b2ddbc07108c2a531
Fernando <fernandoramirez309210@gmail.com>
2025-10-07 19:29:09 -0500

    Fix  export error in Carousel

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Navbar from "./site_components/Navbar";
 import Hero from "./site_components/Hero";
-import Carousel, { DataAnalysisCarousel, SoftwareEngineeringCarousel, UIUXCarousel } from "./site_components/Carousel";
+import SoftwareEngineeringCarousel, { UIUXCarousel, DataAnalysisCarousel } from "./site_components/Carousel";
 import FlipWordsDemo from "./site_components/Footer";
 import SocialLinks from "./site_components/SocialLinks";
 import Cards from "@/components/ui/Cards";
